4823c3c5f30e3564e135848b2ba40b143c07dfe7
KoKoRiolll <kokorioll@foxmail.com>
2021-10-28 14:26:23 +0800

    优化判断条件，删除不必要的代码

---
+++ packages/HangarUploader/fileTypeConfig.js
@@ -0,0 +1,94 @@
+export const fileType = {
+  pdf: {
+    icon: 'file-pdf',
+    color: '#f5222d',
+    pdfPreview: true
+  },
+  png: {
+    icon: 'file-image',
+    color: '#faad14',
+    preview: true
+  },
+  jpg: {
+    icon: 'file-image',
+    color: '#faad14',
+    preview: true
+  },
+  gif: {
+    icon: 'file-image',
+    color: '#faad14',
+    preview: true
+  },
+  psd: {
+    icon: 'file-image',
+    color: '#faad14'
+  },
+  doc: {
+    icon: 'file-word',
+    color: '#1890ff'
+  },
+  docx: {
+    icon: 'file-word',
+    color: '#1890ff'
+  },
+  wps: {
+    icon: 'file-word',
+    color: '#1890ff'
+  },
+  wpt: {
+    icon: 'file-word',
+    color: '#1890ff'
+  },
+  xls: {
+    icon: 'file-excel',
+    color: '#52c41a'
+  },
+  xlsx: {
+    icon: 'file-excel',
+    color: '#52c41a'
+  },
+  et: {
+    icon: 'file-excel',
+    color: '#52c41a'
+  },
+  ett: {
+    icon: 'file-excel',
+    color: '#52c41a'
+  },
+  pptx: {
+    icon: 'file-ppt',
+    color: '#FF9E0E'
+  },
+  ppt: {
+    icon: 'file-ppt',
+    color: '#FF9E0E'
+  },
+  dps: {
+    icon: 'file-ppt',
+    color: '#FF9E0E'
+  },
+  dpt: {
+    icon: 'file-ppt',
+    color: '#FF9E0E'
+  },
+  txt: {
+    icon: 'file-text',
+    color: '#1890ff'
+  },
+  zip: {
+    icon: 'file-zip',
+    color: '#f5222d'
+  },
+  rar: {
+    icon: 'file-zip',
+    color: '#f5222d'
+  },
+  md: {
+    icon: 'file-markdown',
+    color: '#f5222d'
+  },
+  kokoriolll: {
+    icon: 'smile',
+    color: '#3385FF'
+  }
+};
