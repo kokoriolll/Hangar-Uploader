0810eadc3deb56c811dad53ba37f9cf1d0455a86
KoKoRiolll <kokorioll@foxmail.com>
2021-12-06 16:47:35 +0800

    Update code.vue

--- examples/views/code/code.vue
+++ examples/views/code/code.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <hangar-uploader/>
+    <hangar-uploader text="qwe" :value="value"/>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Code'
+    name: 'Code',
+    data() {
+      return {
+        value: ''
+      };
+    },
+    mounted() {
+      setTimeout(() => {
+        this.value = '[{"id":"269BBE73-A3DA-DA11-598D-438DEF04E425","name":"报批申请表迁西福报批申请表迁西福珍全大洪峪铁矿9珍全大洪峪铁矿9.6.pdf"}]';
+      }, 600);
+    }
   };
 </script>
 
